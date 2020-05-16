2dfde8920763bfe7d44fdaeb201477f3d49a882a
Emi Clar <kotaki@gmail.com>
2020-05-15 21:02:24 -0400

    Changed getDay path from /api/days/:id to /api/days/id

--- routes/api/days.js
+++ routes/api/days.js
@@ -5,7 +5,7 @@ const daysController = require("../../controllers/daysController");
 /* GET all days of a plan. Matches with "/api/days" */
 router.get('/', daysController.getAllDays);
 /* GET a single day by id. Matches with "/api/days/:id" */
-router.get("/:id", daysController.getDay);
+router.get("/id", daysController.getDay);
 /* GET the last day (max day) of the plan. Marches with "/api/days/last" */
 router.get("/last", daysController.getLastDay);
 /* POST a day. Matches with "/api/days" */
